bdc0368459db229154399d57d35effca21b4caeb
Amanda Clark <adc0032@auburn.edu>
2022-06-09 00:13:31 -0700

    Update 1_quality_fastqc.sh
    adding additional commenting to scripts

--- Shell_support_scripts/1_quality_fastqc.sh
+++ Shell_support_scripts/1_quality_fastqc.sh
@@ -1,19 +1,27 @@
 #! /bin/bash
 
+## Purpose: The purpose of this script is to gain a sense of quality of the raw data.
+## Input Data: Raw fastq R1 & R2 
+## Output Data: Sample-specific directory with ZIP & HTML files; Download HTML files to view QC results in a web browser
+## More Information: https://www.bioinformatics.babraham.ac.uk/projects/fastqc/INSTALL.txt
+
 # Modules
 module load fastqc/0.11.9
 
-# Variables: working directory(WD), sample(SM), trim status(TS), run id(ID)
+# Variables: working directory(WD), Read file(RD), pre or post clean status(CS), run id(ID)
 WD=${2}
-SM=${1}
-TS=${3}
-ID=`basename ${SM}|awk -F"_" '{print $1}'`
+RD=${1}
+CS=${3}
+ID=`basename ${RD}|awk -F"_" '{print $1}'` #generate a run id variable with the first field of the read name (delimited by underscores)
 
-if [ ! -s ${SM} ]; then
-	echo "${SM} was not found, aborting..."
+# Sanity Check that the read file exists (full path is provided from parent script, hence basename command to strip path in line 15)
+if [ ! -s ${RD} ]; then
+	echo "${RD} was not found, aborting..."
 	exit 1
-elif [[ -s "${WD}/${ID}/${TS}/${ID}_2_fastqc.zip" || -s "${WD}/${ID}/${TS}/${ID}_trim_2P_fastqc.zip" ]]; then
+# Sanity Check that prior FASTQC files have been generated for these data to save on run time & HPC resources. 	
+elif [[ -s "${WD}/${ID}/${CS}/${ID}_2_fastqc.zip" || -s "${WD}/${ID}/${CS}/${ID}_trim_2P_fastqc.zip" ]]; then
 	echo "FASTQC has been run for ${ID} fastq files, moving on..."
 else
-	fastqc --outdir=${WD}/${ID}/${TS} ${WD}/${SM}
+# If not, run fastqc on the specified read file, and provide an output directory based on the SRR ID and pre/post trim status. 
+	fastqc --outdir=${WD}/${ID}/${CS} ${WD}/${RD}
 fi
